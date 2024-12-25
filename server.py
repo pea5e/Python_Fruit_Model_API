f1cadec6236a328f30f41ab17475579f201c4fa2
Salim Alaoui <107775162+pea5e@users.noreply.github.com>
2024-12-25 03:56:48 +0100

    Update server.py

--- server.py
+++ server.py
@@ -11,42 +11,40 @@ CORS(app)
 @app.route('/', methods = ['GET', 'POST']) 
 def home():
     if request.method == 'POST':
-        file = request.json['image']
-        print(file)
-        import base64
-        with open(request.json["filename"], "wb") as fh:
-            fh.write(base64.decodebytes(file))
-        # if file:
-        #     import tensorflow as tf
-        #     import numpy as np
-        #     from os import remove
+        file = request.files['file']
+        if file:
+            import tensorflow as tf
+            import numpy as np
+            from os import remove
 
-        #     def model_prediction(test_image):
-        #         model = tf.keras.models.load_model("fruit_veg_CNN.h5")
-        #         image = tf.keras.preprocessing.image.load_img(test_image, target_size=(64,64))
-        #         input_arr = tf.keras.preprocessing.image.img_to_array(image)
-        #         input_arr = np.array([input_arr])  
-        #         predictions = model.predict(input_arr)
-        #         return np.argmax(predictions)  
+            def model_prediction(test_image):
+                model = tf.keras.models.load_model("fruit_veg_CNN.h5")
+                image = tf.keras.preprocessing.image.load_img(test_image, target_size=(64,64))
+                input_arr = tf.keras.preprocessing.image.img_to_array(image)
+                input_arr = np.array([input_arr])  
+                predictions = model.predict(input_arr)
+                return np.argmax(predictions)  
 
-        #     file.save(file.filename)
-        #     result_index = model_prediction(file.filename)
+            file.save(file.filename)
+            result_index = model_prediction(file.filename)
                     
-        #     with open("labels.txt") as f:
-        #         content = f.readlines()
-        #     labels = [label.strip() for label in content]
+            with open("labels.txt") as f:
+                content = f.readlines()
+            labels = [label.strip() for label in content]
 
-        #     print(f"Prediction: This is a {labels[result_index]}")
-        #     remove(file.filename)
-        #     return jsonify({'data': labels[result_index]})
+            print(f"Prediction: This is a {labels[result_index]}")
+            remove(file.filename)
+            return labels[result_index]
+            return jsonify({'data': labels[result_index]})
             
 
 
     data = "no-file"
+    return data
     return jsonify({'data': data})
   
   
 # driver function 
 if __name__ == '__main__': 
   
-    app.run(host="0.0.0.0",debug = True) 
+    app.run(host="0.0.0.0",debug = False) 
