90368d771330f2ebc98c10f50c8fbb524b51da5e
Salim Alaoui <107775162+pea5e@users.noreply.github.com>
2024-12-25 03:39:28 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request 
+from flask_cors import CORS
+  
+# creating a Flask app 
+app = Flask(__name__) 
+CORS(app)  
+  
+# on the terminal type: curl http://127.0.0.1:5000/ 
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/', methods = ['GET', 'POST']) 
+def home():
+    if request.method == 'POST':
+        file = request.json['image']
+        print(file)
+        import base64
+        with open(request.json["filename"], "wb") as fh:
+            fh.write(base64.decodebytes(file))
+        # if file:
+        #     import tensorflow as tf
+        #     import numpy as np
+        #     from os import remove
+
+        #     def model_prediction(test_image):
+        #         model = tf.keras.models.load_model("fruit_veg_CNN.h5")
+        #         image = tf.keras.preprocessing.image.load_img(test_image, target_size=(64,64))
+        #         input_arr = tf.keras.preprocessing.image.img_to_array(image)
+        #         input_arr = np.array([input_arr])  
+        #         predictions = model.predict(input_arr)
+        #         return np.argmax(predictions)  
+
+        #     file.save(file.filename)
+        #     result_index = model_prediction(file.filename)
+                    
+        #     with open("labels.txt") as f:
+        #         content = f.readlines()
+        #     labels = [label.strip() for label in content]
+
+        #     print(f"Prediction: This is a {labels[result_index]}")
+        #     remove(file.filename)
+        #     return jsonify({'data': labels[result_index]})
+            
+
+
+    data = "no-file"
+    return jsonify({'data': data})
+  
+  
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(host="0.0.0.0",debug = True) 
